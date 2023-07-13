f3ea78ff6bba469b3fb949af775a410f4b372e2a
Santiago Kolac <kolac.santiago1@gmail.com>
2023-07-12 22:10:51 -0300

    Agrego function inicio y botón

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+function inicio() {
+  let cantidadPersonas = 8;
+  let alturaIngresada;
+  let edadIngresada;
+  let mayorDeEdad;
+
+  function evaluarEdad() {
+    edadIngresada = parseInt(prompt("Ingrese su edad"));
+
+    if (edadIngresada >= 18) {
+      mayorDeEdad = true;
+    } else {
+      mayorDeEdad = false;
+    }
+
+    if (mayorDeEdad) {
+      alert("Usted debe abonar $1000");
+    } else {
+      alert("Usted debe abonar $500");
+    }
+  }
+
+  for (let i = 1; i <= cantidadPersonas; i++) {
+    alturaIngresada = parseInt(prompt("Ingrese su altura en cm"));
+
+    if (alturaIngresada >= 160) {
+      alert("Usted puede subir a la montaña rusa");
+      evaluarEdad();
+    } else {
+      alert("Usted no puede subir a la montaña rusa");
+    }
+  }
+}
+
+/*
 let cantidadPersonas = 8;
 let alturaIngresada;
 let edadIngresada;
@@ -31,3 +66,4 @@ window.onload = function () {
     }
   }
 };
+*/
