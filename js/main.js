b8efdba5496ed3c7b95de56b3659c2dbb8ad4e5d
Santiago Kolac <kolac.santiago1@gmail.com>
2023-07-25 12:36:37 -0300

    Agrego arrays, métodos de búsqueda y filtrado y operaciones matemáticas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 function inicio() {
-  let cantidadPersonas = 8;
-  let alturaIngresada;
-  let edadIngresada;
-  let mayorDeEdad;
+  let personas = [];
+  let personasQueEntran = [];
+  const cantidadPersonas = 8;
+  let altura;
+  let edad;
+  let dineroRecaudado;
+  let salario;
 
   function evaluarEdad() {
-    edadIngresada = parseInt(prompt("Ingrese su edad"));
+    while (true) {
+      edad = prompt(`Ingrese su edad`);
 
-    if (edadIngresada >= 18) {
-      mayorDeEdad = true;
-    } else {
-      mayorDeEdad = false;
+      if (!isNaN(edad) && edad >= 0) {
+        break;
+      } else {
+        alert("Por favor, ingrese una edad válida (mayor o igual a 0).");
+      }
     }
 
-    if (mayorDeEdad) {
+    if (edad >= 18) {
       alert("Usted debe abonar $1000");
     } else {
       alert("Usted debe abonar $500");
@@ -21,49 +26,50 @@ function inicio() {
   }
 
   for (let i = 1; i <= cantidadPersonas; i++) {
-    alturaIngresada = parseInt(prompt("Ingrese su altura en cm"));
+    while (true) {
+      altura = prompt(`Persona ${i}: Ingresa su altura`);
 
-    if (alturaIngresada >= 160) {
+      if (!isNaN(altura) && altura >= 0) {
+        break;
+      } else {
+        alert("Por favor, ingrese una altura válida (mayor o igual a 0).");
+      }
+    }
+
+    if (altura >= 160) {
       alert("Usted puede subir a la montaña rusa");
       evaluarEdad();
     } else {
       alert("Usted no puede subir a la montaña rusa");
     }
+
+    let persona = {
+      altura: parseFloat(altura),
+      edad: parseInt(edad),
+    };
+
+    personas.push(persona);
   }
+
+  personasQueEntran = personas.filter((persona) => persona.altura >= 160);
+
+  console.log(personas);
+
+  console.log(personasQueEntran);
+
+  dineroRecaudado = personasQueEntran.reduce((total, persona) => {
+    if (persona.edad >= 18) {
+      return total + 1000;
+    } else {
+      return total + 500;
+    }
+  }, 0);
+
+  alert(
+    "Dinero recaudado en la montaña rusa el día de hoy: $" + dineroRecaudado
+  );
+
+  salario = (dineroRecaudado * 30) / 100;
+
+  alert("El salario del encargado de la fila el día de hoy es de $" + salario);
 }
-
-/*
-let cantidadPersonas = 8;
-let alturaIngresada;
-let edadIngresada;
-let mayorDeEdad;
-
-window.onload = function () {
-  function evaluarEdad() {
-    edadIngresada = parseInt(prompt("Ingrese su edad"));
-
-    if (edadIngresada >= 18) {
-      mayorDeEdad = true;
-    } else {
-      mayorDeEdad = false;
-    }
-
-    if (mayorDeEdad) {
-      alert("Usted debe abonar $1000");
-    } else {
-      alert("Usted debe abonar $500");
-    }
-  }
-
-  for (let i = 1; i <= cantidadPersonas; i++) {
-    alturaIngresada = parseInt(prompt("Ingrese su altura en cm"));
-
-    if (alturaIngresada >= 160) {
-      alert("Usted puede subir a la montaña rusa");
-      evaluarEdad();
-    } else {
-      alert("Usted no puede subir a la montaña rusa");
-    }
-  }
-};
-*/
